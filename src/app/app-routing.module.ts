1039ed329f8236b39b74d5c58b8f34bf6c6b4a30
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-04-30 16:07:15 +0200

    setup initial routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { CharactersComponent } from './pages/characters/characters.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'home-page', pathMatch: 'full' },
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'planets', component: PlanetsComponent },
+  { path: 'characters', component: CharactersComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
 export class AppRoutingModule { }
