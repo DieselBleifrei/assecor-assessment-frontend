c4d7119f4405a2e0fbb2b46c55ded6a34475bf3e
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-04-30 16:08:09 +0200

    added initial layout; added headerbar with routing to initial pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RequestService } from './services/request.service';
+import { StarWarsUniverseAPIs } from './models/swapiStarWarsUniverseAPIs';
+import { SwapiData } from './models/swapiData';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assecor-assessment-frontend';
+
+  private starWarsApis: StarWarsUniverseAPIs;
+
+  constructor(private requestService: RequestService) {
+
+  }
+
+  ngOnInit() {
+    this.requestService.getSwapi().subscribe((data: SwapiData) => {
+      this.starWarsApis = data.result;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderbarComponent } from './layout/headerbar/headerbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WrapperComponent } from './layout/wrapper/wrapper.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent,
+    WrapperComponent,
+    HomePageComponent,
+    MoviesComponent,
+    CharactersComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
