e99e6981663e53e7bb4c450740c3340a548be6cd
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-05-01 01:35:29 +0200

    added movie component

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SwapiMovie } from 'src/app/models/swapiMovie';
+import { SwapiMoviesPage } from 'src/app/models/swapiMoviesPage';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent {
+
+  movies: SwapiMovie[];
+
+  constructor(private http: HttpClient,
+    private dataSharingService: DataSharingService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.dataSharingService.getStarWarsAPIs().subscribe(data => {
+      if (data) {
+        this.http.get<SwapiMoviesPage>(data.films + '?expanded=true').subscribe((movies: SwapiMoviesPage) => {
+          this.movies = movies.result;
+        })
+      }
+    })
+  }
 
 }
