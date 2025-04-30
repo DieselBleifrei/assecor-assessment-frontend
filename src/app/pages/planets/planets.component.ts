68c347039e45f0b20ddb2335942e46ec34cceea9
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-05-01 01:35:43 +0200

    added planets

--- src/app/pages/planets/planets.component.ts
+++ src/app/pages/planets/planets.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SwapiPlanet } from 'src/app/models/swapiPlanet';
+import { SwapiPlanetPage } from 'src/app/models/swapiPlanetPage';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-planets',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./planets.component.scss']
 })
 export class PlanetsComponent {
+
+    planets: SwapiPlanet[];
+  
+    constructor(private http: HttpClient,
+      private dataSharingService: DataSharingService
+    ) {
+  
+    }
+  
+    ngOnInit() {
+      this.dataSharingService.getStarWarsAPIs().subscribe(data => {
+        if (data) {
+          this.http.get<SwapiPlanetPage>(data.planets + '?expanded=true').subscribe((planets: SwapiPlanetPage) => {
+            this.planets = planets.results;
+          })
+        }
+      })
+    }
 
 }
