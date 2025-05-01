8f3d6f428b49eeb4eb294b47394571dccc1b62e6
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-05-01 18:54:56 +0200

    added information to planet information comp

--- src/app/pages/planet-information/planet-information.component.ts
+++ src/app/pages/planet-information/planet-information.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { SwapiPlanet } from 'src/app/models/swapiPlanet';
+import { SwapiPlanetResult } from 'src/app/models/swapiPlanetResult';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-planet-information',
@@ -6,5 +12,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./planet-information.component.scss']
 })
 export class PlanetInformationComponent {
+  
+  planet: SwapiPlanet;
 
+  constructor(private http: HttpClient,
+    private dataSharingService: DataSharingService,
+    private route: ActivatedRoute) {
+  }
+
+
+  ngOnInit() {
+    let combined = combineLatest(this.route.params, this.dataSharingService.getStarWarsAPIs())
+    combined.subscribe(([params, swapiData]) => {
+      if (params && params['id'] && swapiData) {
+        this.http.get<SwapiPlanetResult>(swapiData.planets + '/' + params['id'] + '?expanded=true').subscribe((planet: SwapiPlanetResult) => {
+          this.planet = planet.result;
+        })
+      }
+    })
+  }
 }
