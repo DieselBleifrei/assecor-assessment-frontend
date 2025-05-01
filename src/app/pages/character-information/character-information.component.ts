9f5bf8fc1de67be2a7229b484a87c6f6dd8df438
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-05-01 18:55:57 +0200

    added character information comp information

--- src/app/pages/character-information/character-information.component.ts
+++ src/app/pages/character-information/character-information.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { SwapiCharacter } from 'src/app/models/swapiCharacter';
+import { SwapiCharacterResult } from 'src/app/models/swapiCharacterResult';
+import { DataSharingService } from 'src/app/services/data-sharing.service';
 
 @Component({
   selector: 'app-character-information',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class CharacterInformationComponent {
 
+  character: SwapiCharacter;
+
+  constructor(private http: HttpClient,
+    private dataSharingService: DataSharingService,
+    private route: ActivatedRoute) {
+  }
+
+
+  ngOnInit() {
+    let combined = combineLatest(this.route.params, this.dataSharingService.getStarWarsAPIs())
+    combined.subscribe(([params, swapiData]) => {
+      if (params && params['id'] && swapiData) {
+        this.http.get<SwapiCharacterResult>(swapiData.people + '/' + params['id'] + '?expanded=true').subscribe((character: SwapiCharacterResult) => {
+          this.character = character.result;
+        })
+      }
+    })
+  }
 }
