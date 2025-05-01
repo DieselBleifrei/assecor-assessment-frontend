0181bfbb6107e6b37e983872e4f3171ada5ba42b
Diesel Bleifrei <dieselbleifrei@gmail.com>
2025-05-01 18:59:01 +0200

    added creation dialog trigger to headerbar

--- src/app/layout/headerbar/headerbar.component.ts
+++ src/app/layout/headerbar/headerbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CreateCharacterDialogComponent } from 'src/app/dialogs/create-character-dialog/create-character-dialog.component';
+import { CreateMovieDialogComponent } from 'src/app/dialogs/create-movie-dialog/create-movie-dialog.component';
+import { CreatePlanetDialogComponent } from 'src/app/dialogs/create-planet-dialog/create-planet-dialog.component';
 
 @Component({
   selector: 'app-headerbar',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./headerbar.component.scss']
 })
 export class HeaderbarComponent {
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  openCharacterCreationDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '600px';
+
+    const dialogRef = this.dialog.open(CreateCharacterDialogComponent, dialogConfig);
+  }
+
+  openPlanetCreationDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '600px';
+
+    const dialogRef = this.dialog.open(CreatePlanetDialogComponent, dialogConfig);
+  }
+
+  openMovieCreationDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '600px';
+
+    const dialogRef = this.dialog.open(CreateMovieDialogComponent, dialogConfig);
+  }
 
 }
